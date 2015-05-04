4607fd2d310d2c5f3897052138dddf6a998613cc
int128 <int128@gmail.com>
2015-05-04 18:37:23 +0900

    Add original credit and license of Expect-for-Java

--- src/main/groovy/org/hidetake/groovy/ssh/operation/expect/Expect.groovy
+++ src/main/groovy/org/hidetake/groovy/ssh/operation/expect/Expect.groovy
@@ -1,3 +1,14 @@
+/*
+ * Expect-for-Java
+ * https://github.com/ronniedong/Expect-for-Java
+ *
+ * License:
+ * The use and distribution terms for this software are covered by
+ * the Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php).
+ * By using this software in any fashion, you are agreeing to be bound by
+ * the terms of this license.
+ * You must not remove this notice, or any other, from this software.
+ */
 package org.hidetake.groovy.ssh.operation.expect
 
 import groovy.util.logging.Slf4j
