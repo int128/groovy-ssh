3f43072c3ca9aa410770c49ff1653ffe6f7a86b7
Hidetake Iwata <int128@gmail.com>
2023-01-21 10:07:20 +0900

    Add setup-sshd.sh

---
+++ os-integration-test/setup-sshd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eux
+
+sudo systemctl status sshd
+
+install_host_key () {
+  sudo cp "os-integration-test/etc/ssh/$1" "/etc/ssh/$1"
+  sudo chmod 600 "/etc/ssh/$1"
+}
+
+install_host_key ssh_host_dsa_key
+install_host_key ssh_host_rsa_key
+install_host_key ssh_host_ecdsa_key
+install_host_key ssh_host_ed25519_key
+
+mkdir -p ~/.ssh
+cat os-integration-test/etc/ssh/*.pub > ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
+sudo systemctl restart sshd
